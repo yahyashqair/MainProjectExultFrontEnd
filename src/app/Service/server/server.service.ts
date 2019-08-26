a748f53a40351ec5eeaf2bf425f8e48c1cf3bab9
yahyashqair <37252929+yahyashqair@users.noreply.github.com>
2019-08-26 11:06:04 +0300

    Update server.service.ts

--- src/app/Service/server/server.service.ts
+++ src/app/Service/server/server.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Profile} from '../profile.service';
 
@@ -10,7 +10,7 @@ export class ServerService {
   urlAllServer = 'http://localhost:8080/server/';
   urlReadData = 'http://localhost:8080/server/readData/';
   urlServer = 'http://localhost:8080/server/';
-  serverId;// 0 Local else Not Local
+  serverId; // 0 Local else Not Local
   public getServers() {
     return this.http.get<Server[]>(this.urlAllServer);
   }
@@ -18,20 +18,25 @@ export class ServerService {
   public getServer(id: number) {
     return this.http.get<Server>(this.urlServer + id);
   }
-  public readData(id:number){
-    return this.http.get(this.urlReadData+id);
+
+  public readData(id: number) {
+    return this.http.get(this.urlReadData + id);
   }
 
   public addServer(data: any) {
     return this.http.post<Server[]>(this.urlServer, data);
   }
 
-  public getCurrentServer(){
+  public getCurrentServer() {
     return this.serverId;
   }
 
   constructor(private http: HttpClient) {
-    this.serverId=0;
+    this.serverId = 0;
+  }
+
+  public setCurrentServer(id: number) {
+    this.serverId = id;
   }
 }
 
